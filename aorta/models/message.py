426314113ba8b23f3160329423585741577edfef
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2022-02-09 01:24:14 +0100

    Add dependencies

--- aorta/models/message.py
+++ aorta/models/message.py
@@ -5,5 +5,6 @@ import pydantic
 class Message(pydantic.BaseModel):
     api_version: str = pydantic.Field(..., alias='apiVersion')
     kind: str = pydantic.Field(...)
+    type: str = pydantic.Field(None)
     data: dict = pydantic.Field({})
     spec: dict = pydantic.Field({})
